f9827a23fe34cf0f143c74f85011c6f801044336
s1llhoutte <danikval2000002@gmail.com>
2026-07-06 12:06:06 +0300

    add adminLte

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
+
+Route::get('/admin', AdminController::class)->name('admin.index');
 
 Route::get('/', function () {
     return view('welcome');
