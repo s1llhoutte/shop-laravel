3e10c0525672c31dd2d1c8b31f036b3532d3d76a
s1llhoutte <danikval2000002@gmail.com>
2026-07-06 13:09:40 +0300

    create layout and edit sidebar

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,6 @@
 <?php
 
+use App\Http\Controllers\Main\IndexController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AdminController;
 
-Route::get('/admin', AdminController::class)->name('admin.index');
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', IndexController::class)->name('main.index');
