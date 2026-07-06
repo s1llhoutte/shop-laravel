bc7d0b819b749ef25f410e6a01cbb670f19cbfd8
s1llhoutte <danikval2000002@gmail.com>
2026-07-06 15:18:16 +0300

    add crud for tags, colors and categories

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -149,19 +149,19 @@
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a href="pages/gallery.html" class="nav-link">
+                        <a href="{{ route('category.index') }}" class="nav-link">
                             <i class="nav-icon fas fa-list"></i>
                             <p>Категории</p>
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a href="pages/gallery.html" class="nav-link">
+                        <a href="{{ route('tag.index') }}" class="nav-link">
                             <i class="nav-icon fas fa-tags"></i>
                             <p>Теги</p>
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a href="pages/gallery.html" class="nav-link">
+                        <a href="{{ route('color.index') }}" class="nav-link">
                             <i class="nav-icon fas fa-palette"></i>
                             <p>Цвета</p>
                         </a>
