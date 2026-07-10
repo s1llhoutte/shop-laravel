e548cc035805f6e708cf709b7f0133ac133c0c45
s1llhoutte <danikval2000002@gmail.com>
2026-07-10 15:50:10 +0300

    add crud users

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,12 +10,29 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-#[Fillable(['name', 'email', 'password'])]
+#[Fillable(['name', 'email', 'password', 'surname', 'patronymic', 'age', 'address', 'gender'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
+
+    const GENDER_MALE = 1;
+    const GENDER_FEMALE = 2;
+
+    protected $table = 'users';
+    protected $guarded = false;
+
+    static function GetGenders() {
+        return [
+          self::GENDER_MALE => 'Мужской',
+          self::GENDER_FEMALE => 'Женский'
+        ];
+    }
+
+    public function GetGenderTitleAttribute() {
+        return self::getGenders()[$this->gender];
+    }
 
     /**
      * Get the attributes that should be cast.
