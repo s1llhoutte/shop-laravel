64b1528377502b8184b6ce3fb25215c3d7dd413e
s1llhoutte <danikval2000002@gmail.com>
2026-07-14 18:11:01 +0300

    get connection with vue, add api

---
+++ routes/api.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+
+Route::get('/products', \App\Http\Controllers\API\Product\IndexController::class);
